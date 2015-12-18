f2cfb0b335c2aa859b158081273b0ba56a76ee2d
Bernardo Andreeti <bfa0101@gmail.com>
2015-12-18 21:43:26 -0200

    Versão que está instalada no servidor; 	welcome page: index.xhtml 	credenciais e link de banco de dados do jelastic (alterar se for usar local)

--- src/java/dao/BD.java
+++ src/java/dao/BD.java
@@ -11,7 +11,7 @@ package dao;
  */
 public class BD {
     	public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	public static final String SENHA = "123456";
-	public static final String USUARIO = "root";
-	public static final String URL_CONEXAO = "jdbc:mysql://localhost:3306/EscolaPingoDeGenteDB";
+	public static final String SENHA = "XYFypg49341";
+	public static final String USUARIO = "adminDataBase";
+	public static final String URL_CONEXAO = "jdbc:mysql://node77574-pingodegente.jelasticlw.com.br/EscolaPingoDeGenteDB";
 }
